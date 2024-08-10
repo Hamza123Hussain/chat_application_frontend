ec5c70b0dca3babb5a126d7f1be18c189d00d732
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 20:03:59 +0500

    Demo Version of chat components made and mounted

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import ChatList from '@/components/Chat/ChatList'
+import UserDetail from '@/components/Chat/UserDetail'
 import UserList from '@/components/Chat/UserList'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className=" flex  min-h-screen">
-      <div className=" bg-slate-600 w-6/12 md:w-2/12 border-2">
+      <div className=" bg-slate-600 w-6/12 md:w-2/12 ">
         <UserList />
       </div>
-      <div className=" bg-slate-200 w-6/12 md:w-8/12">aqa</div>
-      <div className=" bg-slate-900 w-1/3 hidden md:w-2/12">qaqs</div>
+      <div className=" bg-slate-200 w-6/12 md:w-8/12">
+        <ChatList />
+      </div>
+      <div className=" bg-slate-900 w-1/3 hidden md:w-2/12">
+        <UserDetail />
+      </div>
     </div>
   )
 }
