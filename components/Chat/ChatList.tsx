3d4df4e5cfdda4edb5dc88630c8db52425dfb9b7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 20:58:17 +0500

    started developing the chat. messages and input to be added

--- components/Chat/ChatList.tsx
+++ components/Chat/ChatList.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import UserInfo from '../User/UserInfo'
 
 const ChatList = () => {
-  return <div></div>
+  return (
+    <div className=" flex flex-col justify-between bg-transparent text-black border-b-2 shadow-sm shadow-black border-black p-2">
+      <UserInfo />
+    </div>
+  )
 }
 
 export default ChatList
