1cbda8807fa73d76a9c1bb965f99575a0c2f92d6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 20:00:59 +0500

    adding the user and chat elements on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import UserList from '@/components/Chat/UserList'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className=" flex  min-h-screen">
-      <div className=" bg-slate-600 w-2/12 border-2 border-red-600">aa</div>
-      <div className=" bg-slate-200 w-8/12">aqa</div>
-      <div className=" bg-slate-900 w-2/12">qaqs</div>
+      <div className=" bg-slate-600 w-6/12 md:w-2/12 border-2">
+        <UserList />
+      </div>
+      <div className=" bg-slate-200 w-6/12 md:w-8/12">aqa</div>
+      <div className=" bg-slate-900 w-1/3 hidden md:w-2/12">qaqs</div>
     </div>
   )
 }
