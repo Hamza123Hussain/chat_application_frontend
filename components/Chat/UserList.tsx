ff759ae262ae32eeca643534103cda811a10c9bd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 02:21:36 +0500

    Removed lines of code and mounted componenets

--- components/Chat/UserList.tsx
+++ components/Chat/UserList.tsx
@@ -1,33 +1,17 @@
-import { Search } from 'lucide-react'
-import Image from 'next/image'
-import React, { useState } from 'react'
+import React from 'react'
+import AddNewUser from '../User/AddNewUser'
+import SearchBar from '../SearchBar'
+import UserImageandName from '../User/UserImageandName'
 
 const UserList = () => {
-  const [searchterm, setsearch] = useState('')
   return (
     <div className=" p-2 flex flex-col">
-      <div className=" flex gap-2 bg-gray-400 rounded-lg p-1 mb-2 items-center w-full hover:border-2  ">
-        {' '}
-        <input
-          type="text"
-          value={searchterm}
-          onChange={(e: any) => setsearch(e.target.value)}
-          placeholder="Search For User"
-          className=" border-none placeholder:text-white text-xs outline-none rounded-lg bg-transparent w-10/12   "
-        />
-        <Search size={20} />
+      <div className=" flex gap-2 items-center ">
+        <SearchBar />
+        <AddNewUser />
       </div>
 
-      <div className=" flex items-center gap-3 hover:bg-gray-500 rounded-lg p-1">
-        <Image
-          src={'/User.png'}
-          alt="USER"
-          width={25}
-          height={25}
-          className=" rounded-full"
-        />
-        <h6 className=" text-[12px]">Hamza Hussain</h6>
-      </div>
+      <UserImageandName />
     </div>
   )
 }
