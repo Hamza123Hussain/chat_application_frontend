4157545ae3dd9f0cfcd0f135b013abcb753eda07
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 03:02:06 +0500

    ROUTING TO THE LOGIN PAGE

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
+'use client'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+  const Router = useRouter()
   return (
     <div className="p-2 flex justify-between items-center bg-green-300 shadow-lg ">
       <div className="flex gap-3 items-center">
@@ -16,7 +19,12 @@ const Navbar = () => {
       </div>
       <div className="flex gap-4">
         {/* Add additional Navbar items here */}
-        <button className="px-6 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition duration-300">
+        <button
+          onClick={() => {
+            Router.push('/Login')
+          }}
+          className="px-6 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition duration-300"
+        >
           Sign In
         </button>
       </div>
