453815382a34b965c953cca8f36dca253dfcce4f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 02:21:52 +0500

    user details layout complete, styling left a bit

--- components/Chat/UserDetail.tsx
+++ components/Chat/UserDetail.tsx
@@ -2,26 +2,19 @@ import Image from 'next/image'
 import React from 'react'
 import UserDetailAccordions from '../User/UserDetailAccordions'
 import UserSettings from '../User/UserSettings'
+import UserImageandName from '../User/UserImageandName'
 
 const UserDetail = () => {
   return (
-    <div className=" flex flex-col gap-7 justify-around">
+    <div className=" flex flex-col gap-3 justify-around">
       {' '}
-      <div className=" flex gap-2 items-center p-2 border-b-2 ">
-        {' '}
-        <Image
-          src={'/User.png'}
-          alt="USER"
-          width={40}
-          height={20}
-          className=" rounded-full"
-        />
-        <h6 className=" text-xl sm:text-sm">Hamza Hussain</h6>
+      <div className=" p-2 border-b-2">
+        <UserImageandName />
       </div>
       <UserSettings />
       <UserDetailAccordions Name="File" /> <UserDetailAccordions Name="Image" />{' '}
       <div className=" p-2 ">
-        <button className=" bg-red-500 text-white px-4 py-2 w-full">
+        <button className=" bg-red-500 text-white px-4 py-2 w-full rounded-lg">
           Block User
         </button>
       </div>
