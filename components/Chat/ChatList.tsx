b17503b8c9c43d65f9f88074f20ec81dc16da4f9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 21:30:22 +0500

    dummy messages plus input field added

--- components/Chat/ChatList.tsx
+++ components/Chat/ChatList.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserInfo from '../User/UserInfo'
-
+import { ChatMessage } from '../Messages'
+import { Camera, Image, Mic } from 'lucide-react'
+const messages = [
+  { text: 'Hi there!', isUser: false },
+  { text: 'Hello!', isUser: true },
+  { text: 'How are you?', isUser: false },
+  { text: 'I’m good, thanks!', isUser: true },
+]
 const ChatList = () => {
+  const [messageinput, setmessageinput] = useState('')
   return (
-    <div className=" flex flex-col justify-between bg-transparent text-black border-b-2 shadow-sm shadow-black border-black p-2">
+    <div className=" flex flex-col justify-between ">
       <UserInfo />
+      <div className=" flex-1">
+        {messages.map((msg, index) => (
+          <ChatMessage key={index} message={msg.text} isUser={msg.isUser} />
+        ))}
+      </div>
+      <div className=" bg-slate-600 w-full flex justify-between items-center gap-2  p-2 ">
+        <div className=" flex items-center gap-2">
+          <Image /> <Camera /> <Mic />
+        </div>
+        <input
+          value={messageinput}
+          onChange={(e: any) => setmessageinput(e.target.value)}
+          type="text"
+          className=" border-2  outline-none bg-transparent flex-1 rounded-lg p-1  "
+        />
+        <button className=" bg-blue-500 rounded-lg px-5 py-2">Send</button>
+      </div>
     </div>
   )
 }
