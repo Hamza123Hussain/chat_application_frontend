2c35e1800431ab5b62caba09673164cb252906bc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 02:19:58 +0500

    merged lines of code into a componenet

--- components/User/UserInfo.tsx
+++ components/User/UserInfo.tsx
@@ -1,21 +1,12 @@
 import { Info, Phone, Video } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import UserImageandName from './UserImageandName'
 
 const UserInfo = () => {
   return (
     <div className=" flex justify-between bg-transparent text-black border-b-2 shadow-sm shadow-black border-black p-2">
-      <div className=" flex gap-2 items-center ">
-        {' '}
-        <Image
-          src={'/User.png'}
-          alt="USER"
-          width={25}
-          height={25}
-          className=" rounded-full"
-        />
-        <h6 className=" text-[10px] sm:text-sm">Hamza Hussain</h6>
-      </div>
+      <UserImageandName />
       <div className=" flex gap-5 items-center cursor-pointer">
         <Phone size={20} />
         <Video size={20} />
