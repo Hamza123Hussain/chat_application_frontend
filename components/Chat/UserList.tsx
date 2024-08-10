e380647fb2f20efd0b468622b3b6fb026b1a9609
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 20:58:48 +0500

    Added A searchbar and manipulating through usestate

--- components/Chat/UserList.tsx
+++ components/Chat/UserList.tsx
@@ -1,9 +1,23 @@
+import { Search } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const UserList = () => {
+  const [searchterm, setsearch] = useState('')
   return (
     <div className=" p-2 flex flex-col">
+      <div className=" flex gap-2 bg-gray-400 rounded-lg p-1 mb-2 items-center w-full hover:border-2  ">
+        {' '}
+        <input
+          type="text"
+          value={searchterm}
+          onChange={(e: any) => setsearch(e.target.value)}
+          placeholder="Search For User"
+          className=" border-none placeholder:text-white text-xs outline-none rounded-lg bg-transparent w-10/12   "
+        />
+        <Search size={20} />
+      </div>
+
       <div className=" flex items-center gap-3 hover:bg-gray-500 rounded-lg p-1">
         <Image
           src={'/User.png'}
