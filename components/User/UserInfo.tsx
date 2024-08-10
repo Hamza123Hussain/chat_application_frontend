b54a6ad8e52037ef9a8c5807a0eff1e2f519a40c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-10 21:00:15 +0500

    User Info Componenet Made that shows the user's name and image on the chat

---
+++ components/User/UserInfo.tsx
@@ -0,0 +1,28 @@
+import { Info, Phone, Video } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+const UserInfo = () => {
+  return (
+    <div className=" flex justify-between">
+      <div className=" flex gap-2 items-center ">
+        {' '}
+        <Image
+          src={'/User.png'}
+          alt="USER"
+          width={25}
+          height={25}
+          className=" rounded-full"
+        />
+        <h6 className=" text-[10px] sm:text-sm">Hamza Hussain</h6>
+      </div>
+      <div className=" flex gap-5 items-center cursor-pointer">
+        <Phone size={20} />
+        <Video size={20} />
+        <Info size={20} />
+      </div>
+    </div>
+  )
+}
+
+export default UserInfo
