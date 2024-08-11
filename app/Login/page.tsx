438f6b0f28a13b0e2c00ae02d1a2fce4979d344b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 18:43:53 +0500

    Removed Login Function and added in a seperate file

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,7 +1,5 @@
 'use client'
-import { auth, db } from '@/utils/FirebaseConfig'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { doc, setDoc } from 'firebase/firestore'
+import { HandleLogin } from '@/functions/LoginUser'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
@@ -13,29 +11,6 @@ const Login = () => {
   const Router = useRouter()
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputVal((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-  }
-
-  const handleSubmit = async () => {
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        inputVal.email,
-        inputVal.password
-      )
-      if (userCredential) {
-        await setDoc(doc(db, 'Users', userCredential.user.uid), {
-          Email: inputVal.email,
-          id: userCredential.user.uid,
-          Blocked: [],
-        })
-        await setDoc(doc(db, 'Chats', userCredential.user.uid), {
-          Chats: [],
-        })
-        alert('User registered successfully')
-      }
-    } catch (error) {
-      console.error('Error during registration:', error)
-    }
   }
 
   return (
@@ -57,10 +32,10 @@ const Login = () => {
         className="mb-2 p-2 rounded"
       />
       <button
-        onClick={handleSubmit}
+        onClick={() => HandleLogin(inputVal, Router)}
         className="bg-green-300 px-5 py-1 rounded "
       >
-        Sign Up
+        Login In
       </button>
       <h6 className=" text-xs  mt-2 text-gray-400 text-center">
         Already Have A Account? Click Here To{' '}
