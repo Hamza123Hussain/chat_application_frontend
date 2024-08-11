485b40920a2a6318fcd0b1c1cda6ddc58480a71a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-11 17:52:46 +0500

    Added a file input so that user can upload their images as user profile

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,29 +1,46 @@
 'use client'
 import { auth, db } from '@/utils/FirebaseConfig'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth'
 import { doc, setDoc } from 'firebase/firestore'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useState, ChangeEvent } from 'react'
 
-const SignUp = () => {
-  const [inputVal, setInputVal] = useState({
+interface InputValues {
+  email: string
+  password: string
+  Name: string
+  File: File | null
+}
+
+const SignUp: React.FC = () => {
+  const [inputVal, setInputVal] = useState<InputValues>({
     email: '',
     password: '',
     Name: '',
+    File: null,
   })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputVal((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    console.log(inputVal)
   }
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setInputVal((prev) => ({ ...prev, File: e.target.files![0] }))
+    }
+  }
+
   const Router = useRouter()
 
   const handleSubmit = async () => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        inputVal.email,
-        inputVal.password
-      )
+      const userCredential: UserCredential =
+        await createUserWithEmailAndPassword(
+          auth,
+          inputVal.email,
+          inputVal.password
+        )
       if (userCredential) {
         await setDoc(doc(db, 'Users', userCredential.user.uid), {
           Name: inputVal.Name,
@@ -43,6 +60,13 @@ const SignUp = () => {
 
   return (
     <div className="flex flex-col bg-slate-700 p-3 min-h-screen">
+      <input
+        type="file"
+        name="File"
+        onChange={handleFileChange}
+        className="mb-2 p-2 rounded"
+      />
+
       <input
         type="text"
         placeholder="Enter Name"
@@ -70,10 +94,13 @@ const SignUp = () => {
       <button onClick={handleSubmit} className="bg-green-300 px-5 py-1 rounded">
         Sign Up
       </button>
-      <h6 className=" text-xs text-center text-gray-400 mt-2">
-        Already Have A Account? Click Here To{' '}
-        <span onClick={() => Router.push('/Login')} className=" underline">
-          Login in
+      <h6 className="text-xs text-center text-gray-400 mt-2">
+        Already Have An Account? Click Here To{' '}
+        <span
+          onClick={() => Router.push('/Login')}
+          className="underline cursor-pointer"
+        >
+          Log In
         </span>
       </h6>
     </div>
