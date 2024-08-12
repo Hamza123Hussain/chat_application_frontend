c460ec44337e4d7c9833e732bb6dfb58ad2c226f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 02:54:16 +0500

    function for saerch and storing results

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,28 @@
+import { Getusers } from '@/functions/GetUsersBySearch'
+import { useUserContext } from '@/utils/Context'
 import { Search } from 'lucide-react'
 import React, { useState } from 'react'
 
 const SearchBar = () => {
+  const [loading, setloading] = useState(false)
+  const { setusers } = useUserContext()
   const [searchterm, setsearch] = useState('')
+  const GetPeople = async () => {
+    setloading(true)
+    try {
+      const data = await Getusers(searchterm)
+      if (data) {
+        setusers(data)
+        setloading(false)
+      }
+    } catch (error) {
+      console.log(error)
+      setloading(false)
+    }
+  }
+  if (loading) {
+    return <div className="loader"></div>
+  }
   return (
     <div className=" flex gap-2 bg-gray-400 rounded-lg p-1 mb-2 items-center w-full hover:border-2  ">
       {' '}
@@ -13,7 +33,7 @@ const SearchBar = () => {
         placeholder="Search For User"
         className=" border-none placeholder:text-white text-xs outline-none rounded-lg bg-transparent w-11/12   "
       />
-      <Search size={20} />
+      <Search onClick={GetPeople} size={20} />
     </div>
   )
 }
