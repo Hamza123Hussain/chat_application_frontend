af8930ba76499e207a8594786645d2bed79b70cd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 14:46:31 +0500

    not needed user details on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,10 @@
 import ChatList from '@/components/Chat/ChatList'
 import UserDetail from '@/components/Chat/UserDetail'
 import UserList from '@/components/Chat/UserList'
-import { auth } from '@/utils/FirebaseConfig'
-import { User } from 'firebase/auth'
-import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [user, setUser] = useState<User | null>(null)
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user)
-      } else {
-        setUser(null)
-      }
-    })
-
-    return () => unsubscribe()
-  }, [])
-
-  if (!user) {
-    return <div>No user is logged in</div>
-  }
-
   return (
     <div className=" flex  min-h-screen">
-      <p>User ID: {user.uid}</p>
-      <p>Name: {user.displayName}</p>
-      <p>Email: {user.email}</p>
       <div className=" bg-slate-600 w-6/12 md:w-2/12 ">
         <UserList />
       </div>
