b7d49ba3fdfdad9d333a8dadf03d708278713dfe
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 20:06:07 +0500

    getting the data of current user when logged in

---
+++ functions/GettingUserDetail.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+// Async function to fetch and handle user details
+export const fetchAndHandleUserDetails = async (UserId: string) => {
+  try {
+    const Response = await axios.get(
+      `http://localhost:5000/api/GetUser/?userId=${UserId}`
+    )
+    if (Response.status === 200) {
+      return Response.data
+    }
+  } catch (error) {
+    console.log(`ERROR IN GETTING DATA ${error}`)
+  }
+}
