c1c75c538ed50e2a0d8ec51d6a5b5bfc1b4f6b97
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 16:25:28 +0500

    getting data from node backend

--- functions/CreatingUser.ts
+++ functions/CreatingUser.ts
@@ -1,43 +1,15 @@
-import { auth, db, storage } from '@/utils/FirebaseConfig'
 import { InputValues } from '@/utils/SignUpInterface'
-import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth'
-import { doc, setDoc } from 'firebase/firestore'
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { showCustomToast } from './CustomToast'
+import axios from 'axios'
 export const handleSubmit = async (inputVal: InputValues) => {
   try {
-    const userCredential: UserCredential = await createUserWithEmailAndPassword(
-      auth,
-      inputVal.email,
-      inputVal.password
-    )
-
-    if (userCredential) {
-      let fileURL = ''
-
-      // Upload the file to Firebase Storage
-      if (inputVal.File) {
-        const fileRef = ref(
-          storage,
-          `users/${userCredential.user.uid}/${inputVal.File.name}`
-        )
-        await uploadBytes(fileRef, inputVal.File)
-        fileURL = await getDownloadURL(fileRef)
+    const Response = await axios.post(
+      'http://localhost:5000/api/User/Register',
+      {
+        inputVal,
       }
-
-      // Save the user data to Firestore
-      await setDoc(doc(db, 'Users', userCredential.user.uid), {
-        Name: inputVal.Name,
-        Email: inputVal.email,
-        id: userCredential.user.uid,
-        Blocked: [],
-        FileURL: fileURL, // Store the download URL in Firestore
-      })
-
-      await setDoc(doc(db, 'Chats', userCredential.user.uid), {
-        Chats: [],
-      })
-
+    )
+    if (Response.status == 200) {
       showCustomToast()
     }
   } catch (error) {
