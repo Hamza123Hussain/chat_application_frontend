2eacc9c8244819514b377af27e7b3d7d79032f25
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-12 14:46:54 +0500

    getting user in the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
 'use client'
+import { auth } from '@/utils/FirebaseConfig'
+import { User } from 'firebase/auth'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CurrentUser from './User/CurrentUser'
 
 const Navbar = () => {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user)
+      } else {
+        setUser(null)
+      }
+    })
+
+    return () => unsubscribe()
+  }, [])
   const Router = useRouter()
   return (
     <div className="p-2 flex justify-between items-center bg-green-300 shadow-lg ">
@@ -18,15 +34,18 @@ const Navbar = () => {
         <h1 className="text-lg font-semibold text-white">ChatStream</h1>
       </div>
       <div className="flex gap-4">
-        {/* Add additional Navbar items here */}
-        <button
-          onClick={() => {
-            Router.push('/Login')
-          }}
-          className="px-6 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition duration-300"
-        >
-          Sign In
-        </button>
+        {user ? (
+          <CurrentUser User={user} />
+        ) : (
+          <button
+            onClick={() => {
+              Router.push('/Login')
+            }}
+            className="px-6 py-2 rounded-md bg-green-500 text-white hover:bg-green-600 transition duration-300"
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </div>
   )
