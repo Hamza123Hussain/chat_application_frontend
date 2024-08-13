487b8265146038994d4362de80259b4567fccfad
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 18:55:05 +0500

    Added a new state in context. soon it will converted through redux

--- utils/Context.tsx
+++ utils/Context.tsx
@@ -10,12 +10,18 @@ import React, {
 const UserContext = createContext<any>(null)
 
 export const ContextProvider = ({ children }: { children: ReactNode }) => {
+  const [chatData, setChatData] = useState<any[]>(() => {
+    // Load chatData from localStorage, if available
+    const storedChatData = localStorage.getItem('chatData')
+    return storedChatData ? JSON.parse(storedChatData) : [] // Parse JSON or default to an empty array
+  })
   const [userID, setID] = useState<string>(() => {
     // Retrieve userID from localStorage, if available
     const storedID = localStorage.getItem('userID')
     return storedID || '' // Default to an empty string if no userID is found
   })
   const [searchUsers, setusers] = useState([])
+
   useEffect(() => {
     // Store userID in localStorage whenever it changes
     if (userID) {
@@ -25,8 +31,15 @@ export const ContextProvider = ({ children }: { children: ReactNode }) => {
     }
   }, [userID])
 
+  useEffect(() => {
+    // Store chatData in localStorage whenever it changes
+    localStorage.setItem('chatData', JSON.stringify(chatData))
+  }, [chatData])
+
   return (
-    <UserContext.Provider value={{ userID, setID, searchUsers, setusers }}>
+    <UserContext.Provider
+      value={{ userID, setID, searchUsers, setusers, chatData, setChatData }}
+    >
       {children}
     </UserContext.Provider>
   )
