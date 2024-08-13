5c3730c2993ae6e1682032841b21782c79f8110c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 16:04:33 +0500

    Getting the UserChats

--- components/Chat/UserList.tsx
+++ components/Chat/UserList.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AddNewUser from '../User/AddNewUser'
 import SearchBar from '../SearchBar'
 import UserImageandName from '../User/UserImageandName'
+import { connectToWebSocket } from '@/functions/CreatingChatList'
+import { useUserContext } from '@/utils/Context'
 
 const UserList = () => {
+  const { userID } = useUserContext()
+  useEffect(() => {
+    // Function to handle creating a new chat and connecting to the WebSocket
+    const initializeChat = async () => {
+      // Then connect to the WebSocket to listen for updates
+      connectToWebSocket(userID)
+    }
+
+    // Call the initialization function when the component mounts
+    initializeChat()
+  }, [userID]) // The effect will re-run if the userID changes
   return (
     <div className=" p-2 flex flex-col">
       <div className=" flex gap-2 items-center ">
