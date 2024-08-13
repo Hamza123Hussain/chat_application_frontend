c69cf13071c37501c8e97a4eb73b0169d6078798
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-13 19:16:50 +0500

    Function to Get Chat from the API

---
+++ functions/GettingAChat.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const GetChat = async (ChatID: string) => {
+  try {
+    const Response = await axios.get(
+      `http://localhost:5000/api/Chats/Chat?ChatID=${ChatID}`
+    )
+    if (Response.status === 200) {
+      console.log('CHAT OF USER', Response.data)
+    }
+  } catch (error) {}
+}
