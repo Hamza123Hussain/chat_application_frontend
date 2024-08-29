d2ad4a3cf5ec0e0124f8698d4e866dacc79e16c8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 18:57:07 +0500

    fixed the path name and getting the chat data when clicked on the user profile

--- functions/GettingAChat.ts
+++ functions/GettingAChat.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const GetChat = async (ChatID: string) => {
   try {
     const Response = await axios.get(
-      `http://localhost:5000/api/Chats/GetChat?ChatID=${ChatID}`
+      `http://localhost:5000/api/Chats/GetChatOnce?ChatID=${ChatID}`
     )
     if (Response.status === 200) {
       console.log('CHAT OF USER', Response.data)
