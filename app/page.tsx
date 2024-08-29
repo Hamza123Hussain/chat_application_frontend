8bb3344bf13ab362da98cf5721dfa24dab015113
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 18:10:26 +0500

    removed userdetail for now

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ export default function Home() {
       <div className="bg-slate-600 w-6/12 md:w-2/12">
         <UserList />
       </div>
-      <div className="bg-slate-200 w-6/12 md:w-8/12">{/* <ChatList /> */}</div>
-      <div className="bg-slate-500 w-1/12 md:w-2/12">
-        {/* <UserDetail /> */}
-      </div>
+      <div className="bg-slate-200 w-6/12 md:w-10/12">{/* <ChatList /> */}</div>
     </div>
   )
 }
+{
+  /* <div className="bg-slate-500 w-1/12 md:w-2/12">
+{/* <UserDetail /> */
+}
+// </div> */}
