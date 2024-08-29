6582a7fc3c1d61d57d371882d6ae29c0452a2625
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 18:57:53 +0500

    work has to be done here

--- components/User/UserInfo.tsx
+++ components/User/UserInfo.tsx
@@ -1,8 +1,6 @@
 import { Info, Phone, Video } from 'lucide-react'
-import Image from 'next/image'
 import React from 'react'
 import UserImageandName from './UserImageandName'
-
 const UserInfo = () => {
   return (
     <div className=" flex justify-between bg-transparent text-black border-b-2 shadow-sm shadow-black border-black p-2">
@@ -15,5 +13,4 @@ const UserInfo = () => {
     </div>
   )
 }
-
 export default UserInfo
