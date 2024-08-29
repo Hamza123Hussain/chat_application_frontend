d9ec2c0730bd96b8285984ee6a50edf1a144e121
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 07:56:58 +0500

    styling has to be done

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 'use client'
-import ChatList from '@/components/Chat/ChatList'
-import UserDetail from '@/components/Chat/UserDetail'
+import React, { useEffect, useRef } from 'react'
 import UserList from '@/components/Chat/UserList'
-
 export default function Home() {
   return (
-    <div className=" flex  min-h-screen">
-      <div className=" bg-slate-600 w-6/12 md:w-2/12 ">
+    <div className="flex min-h-screen">
+      <div className="bg-slate-600 w-6/12 md:w-2/12">
         <UserList />
       </div>
-      <div className=" bg-slate-200 w-6/12 md:w-8/12">
-        <ChatList />
-      </div>
-      <div className=" bg-slate-500 w-1/12  md:w-2/12">
-        <UserDetail />
+      <div className="bg-slate-200 w-6/12 md:w-8/12">{/* <ChatList /> */}</div>
+      <div className="bg-slate-500 w-1/12 md:w-2/12">
+        {/* <UserDetail /> */}
       </div>
     </div>
   )
