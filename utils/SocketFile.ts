f4e5ef152f3a829c721ceb0e6f0ce0872f407c3f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-29 19:12:03 +0500

    this file will be deleted soon

--- utils/SocketFile.ts
+++ utils/SocketFile.ts
@@ -7,14 +7,3 @@ const SOCKET_SERVER_URL = 'http://localhost:5000'
 const socket: Socket = io(SOCKET_SERVER_URL)
 
 export default socket
-
-/**    socket.emit('Chat', 'ezisEbOzjcS1aFzX21Tp')
-
-    // Listener for 'MessageReceived' event from the server
-    socket.on('ChatData', (data) => {
-      console.log('Received data from server:', data)
-
-      // Process the received chat data here
-    })
-
-    // Cleanup on component unmount */
