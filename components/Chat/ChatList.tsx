d66ac5d1dba48ae6bb936367fab02772af663d68
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-30 05:46:55 +0500

    cleaning the code and divided it into different componenets

--- components/Chat/ChatList.tsx
+++ components/Chat/ChatList.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useRef } from 'react'
-import { FiImage, FiCamera, FiMic } from 'react-icons/fi'
 import { ChatMessage } from '../Messages'
 import { io } from 'socket.io-client'
 import { Message } from '@/utils/MessageInterface'
+import NewMessage from '../NewMessage'
+import { useUserContext } from '@/utils/Context'
 export default function ChatList() {
+  const { chatID } = useUserContext()
+  console.log('CHAT ID ', chatID)
   const [messages, setmessage] = useState<Message[]>([])
-  const [messageInput, setMessageInput] = useState('')
   const messagesEndRef = useRef<HTMLDivElement | null>(null)
   useEffect(() => {
     if (messagesEndRef.current) {
@@ -27,7 +29,6 @@ export default function ChatList() {
   }, [])
   return (
     <div className="flex flex-col justify-between h-full">
-      {/* Messages */}
       <div
         className="flex-1 overflow-y-auto p-4 space-y-4"
         style={{ maxHeight: 'calc(100vh - 160px)' }}
@@ -37,24 +38,7 @@ export default function ChatList() {
         ))}
         <div ref={messagesEndRef} />
       </div>
-      {/* Input Field */}
-      <div className="bg-white border-t border-gray-300 flex items-center gap-2 p-2 sticky bottom-0">
-        <div className="flex items-center gap-2">
-          <FiImage className="text-gray-500 hover:text-gray-700 cursor-pointer" />
-          <FiCamera className="text-gray-500 hover:text-gray-700 cursor-pointer" />
-          <FiMic className="text-gray-500 hover:text-gray-700 cursor-pointer" />
-        </div>
-        <input
-          value={messageInput}
-          onChange={(e) => setMessageInput(e.target.value)}
-          type="text"
-          placeholder="Type a message..."
-          className="flex-1 border-2 border-gray-300 rounded-lg p-2 outline-none focus:border-blue-500"
-        />
-        <button className="bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600 transition duration-300">
-          Send
-        </button>
-      </div>
+      <NewMessage />
     </div>
   )
 }
