449418df993a888536ba82c253b84dd2aade2bea
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 03:42:07 +0500

    added all the required info needed in the message

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,44 +1,47 @@
 import { useUserContext } from '@/utils/Context'
+import { Message } from '@/utils/MessageInterface'
 import Image from 'next/image'
 import React from 'react'
-
-export const ChatMessage = ({
-  message,
-  isUser,
-  Pic,
-}: {
-  message: string
-  isUser: string
-  Pic: string | null
-}) => {
+export const ChatMessage = ({ messageData }: { messageData: Message }) => {
   const { userID } = useUserContext()
-
   return (
     <div
-      className={`flex ${
-        isUser === userID ? 'justify-end' : 'justify-start'
+      className={`${
+        messageData.senderId === userID ? 'justify-end' : 'justify-start'
       } my-2`}
     >
       <div
-        className={`p-2 max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg rounded-lg shadow-md ${
-          isUser === userID
+        className={`p-2  max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg rounded-lg shadow-md flex flex-col ${
+          messageData.senderId === userID
             ? 'bg-blue-500 text-white'
             : 'bg-gray-200 text-gray-800'
         }`}
       >
-        {Pic && (
+        <div className=" pl-2 mb-3 flex items-center gap-2">
+          {messageData.UserProfileImage && (
+            <Image
+              className=" rounded-full"
+              src={messageData.UserProfileImage}
+              width={20}
+              height={20}
+              alt={messageData.Name}
+            />
+          )}
+          <h6>{messageData.Name}</h6>
+        </div>
+        {messageData.MessageImage && (
           <div className="w-full mb-2">
             <Image
-              src={Pic}
+              src={messageData.MessageImage}
               width={300}
               height={200}
-              alt={message}
+              alt={messageData.text}
               className="rounded-lg w-full "
               layout="responsive"
             />
           </div>
         )}
-        <p className="text-sm">{message}</p>
+        <p className="text-sm pl-3">{messageData.text}</p>
       </div>
     </div>
   )
