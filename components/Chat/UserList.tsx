18904fba3d0a4e84c8ed945dcd316d47ff6d7ce2
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 07:23:41 +0500

    loader will be fixed

--- components/Chat/UserList.tsx
+++ components/Chat/UserList.tsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import AddNewUser from '../User/AddNewUser'
-import SearchBar from '../SearchBar'
 import { useUserContext } from '@/utils/Context'
 import { io } from 'socket.io-client'
 import { CHATDAT } from '@/utils/ChatDataInterface'
 import UserListCard from '../User/UserListCard'
-
+import Loader from '../Loader'
 const UserList: React.FC = () => {
   const { userID, Flag, MessageFlag } = useUserContext()
   const [chatData, setChatData] = useState<CHATDAT[]>([])
-
+  const [Loading, setloading] = useState(false)
   useEffect(() => {
+    setloading(true)
     const socket = io('http://localhost:5000')
     socket.emit('UserList', userID)
     socket.on('UserListReceived', (data: any) => {
       // console.log('Received data from server:', data)
       setChatData(data)
+      setloading(false)
     })
     return () => {
       if (socket) {
@@ -23,12 +24,11 @@ const UserList: React.FC = () => {
       }
     }
   }, [userID, Flag, MessageFlag])
-
+  if (Loading) return <Loader />
   return (
     <div className="p-2 flex flex-col">
       <div className="flex gap-2 items-center">
         {/* <SearchBar /> */}
-
         <AddNewUser />
       </div>
       <div className="mt-4">
@@ -39,5 +39,4 @@ const UserList: React.FC = () => {
     </div>
   )
 }
-
 export default UserList
