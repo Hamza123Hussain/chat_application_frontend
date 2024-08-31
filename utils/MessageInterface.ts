e1e5a247f84d1de5b9eaf1dc06ae815fa0c06599
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 07:21:51 +0500

    GAVE ANY TYPE AS OTHER WAS CAUSING ERROR

--- utils/MessageInterface.ts
+++ utils/MessageInterface.ts
@@ -1,13 +1,9 @@
 export interface Message {
   senderId: string
   text: string
-  timestamp: FirestoreTimestamp
+  timestamp: any
   MessageImage: string | null
   UserProfileImage: string | null
   Name: string
   MessageID: string
-}
-type FirestoreTimestamp = {
-  seconds: number
-  nanoseconds: number
 }
