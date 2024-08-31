70e154919a46427aaa853954e491b8333bd2f856
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 07:22:16 +0500

    NOT STORING CHATID IN TH local storage

--- utils/Context.tsx
+++ utils/Context.tsx
@@ -11,11 +11,7 @@ export const ContextProvider = ({ children }: { children: ReactNode }) => {
   const [Flag, setFlag] = useState(false)
   const [RecieverID, setRecieverID] = useState('')
   const [MessageFlag, setMessageFlag] = useState(false)
-  const [chatID, setCHATID] = useState<string>(() => {
-    // Retrieve chatID from localStorage on initialization
-    const storedChatID = localStorage.getItem('chatID')
-    return storedChatID || '' // Default to an empty string if no chatID is found
-  })
+  const [chatID, setCHATID] = useState<string>()
   const [userID, setID] = useState<string>(() => {
     // Retrieve userID from localStorage on initialization
     const storedID = localStorage.getItem('userID')
@@ -30,14 +26,6 @@ export const ContextProvider = ({ children }: { children: ReactNode }) => {
       localStorage.removeItem('userID')
     }
   }, [userID])
-
-  useEffect(() => {
-    if (chatID) {
-      localStorage.setItem('chatID', chatID)
-    } else {
-      localStorage.removeItem('chatID')
-    }
-  }, [chatID])
 
   return (
     <UserContext.Provider
