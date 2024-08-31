187650b0527aaf6eec4506316c3a04a2359f2083
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 19:15:15 +0500

    removing user from the search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,21 +3,22 @@ import { useUserContext } from '@/utils/Context'
 import { Search } from 'lucide-react'
 import React, { useState } from 'react'
 import Loader from './Loader'
-
 const SearchBar = () => {
   const [loading, setloading] = useState(false)
-  const { setusers } = useUserContext()
+  const { setusers, userID } = useUserContext()
   const [searchterm, setsearch] = useState('')
   const GetPeople = async () => {
     try {
       setloading(true)
       const data = await Getusers(searchterm)
       if (data) {
-        setusers(data)
-        setloading(false)
+        // Filter out the user with the matching userID
+        const filteredData = data.filter((user: any) => user.id !== userID)
+        setusers(filteredData)
       }
     } catch (error) {
       console.log(error)
+    } finally {
       setloading(false)
     }
   }
@@ -25,18 +26,20 @@ const SearchBar = () => {
     return <Loader />
   }
   return (
-    <div className=" flex gap-2 bg-gray-400 rounded-lg p-1 mb-2 items-center w-full hover:border-2  ">
-      {' '}
+    <div className="flex gap-2 bg-gray-800 rounded-lg p-2 mb-2 items-center w-full hover:border-2 hover:border-green-500 transition-colors duration-300">
       <input
         type="text"
         value={searchterm}
         onChange={(e: any) => setsearch(e.target.value)}
         placeholder="Search For User"
-        className=" border-none placeholder:text-white text-xs outline-none rounded-lg bg-transparent w-11/12   "
+        className="border-none placeholder:text-gray-400 text-white text-xs outline-none rounded-lg bg-transparent w-11/12"
       />
-      <Search onClick={GetPeople} size={20} />
+      <Search
+        onClick={GetPeople}
+        size={20}
+        className="text-gray-400 hover:text-green-500 transition-colors duration-300"
+      />
     </div>
   )
 }
-
 export default SearchBar
