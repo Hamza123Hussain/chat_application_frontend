b10ba23b727f7eebb7e6f82c170444a917c9b2b9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 07:22:42 +0500

    this takes the timestamp and gives us the real time with pm or am back

--- functions/FormatingTime.ts
+++ functions/FormatingTime.ts
@@ -1,20 +1,10 @@
-function formatTimestampToAmPm(timestamp: FirestoreTimestamp): string {
-  // Convert Firestore timestamp to milliseconds
-  const date = new Date(
-    timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
-  )
+// Convert Firestore timestamp to Date
+export const timestampToDate = (timestamp: any) => {
+  const seconds = timestamp.seconds
+  return new Date(seconds * 1000) // Convert seconds to milliseconds
+}
 
-  // Extract hours, minutes, and AM/PM period
-  let hours = date.getHours()
-  const minutes = date.getMinutes()
-  const amPm = hours >= 12 ? 'PM' : 'AM'
-
-  // Convert 24-hour format to 12-hour format
-  hours = hours % 12 || 12 // Adjust hours for 12-hour format
-
-  // Format minutes to always be two digits
-  const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
-
-  // Return the formatted time
-  return `${hours}:${formattedMinutes} ${amPm}`
+// Format Date object to 24-hour time
+export const formatTime = (date: any) => {
+  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
 }
