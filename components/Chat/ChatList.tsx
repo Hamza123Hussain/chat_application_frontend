84b23357a7e202c4d023f05fe6f858630e8ffa4c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 05:13:11 +0500

    sending cahtid now instead of dummy data to the api

--- components/Chat/ChatList.tsx
+++ components/Chat/ChatList.tsx
@@ -4,21 +4,22 @@ import { io } from 'socket.io-client'
 import { Message } from '@/utils/MessageInterface'
 import NewMessage from '../NewMessage'
 import { useUserContext } from '@/utils/Context'
+
 export default function ChatList() {
   const { chatID, MessageFlag } = useUserContext()
-  console.log('CHAT ID ', chatID)
   const [messages, setmessage] = useState<Message[]>([])
   const messagesEndRef = useRef<HTMLDivElement | null>(null)
+
   useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }, [messages])
+
   useEffect(() => {
     const socket = io('http://localhost:5000')
-    socket.emit('Chat', 'ezisEbOzjcS1aFzX21Tp')
+    socket.emit('Chat', chatID)
     socket.on('ChatData', (data) => {
-      console.log('Received data from server:', data.messages)
       setmessage(data.messages)
     })
     return () => {
@@ -26,9 +27,10 @@ export default function ChatList() {
         socket.disconnect()
       }
     }
-  }, [MessageFlag])
+  }, [MessageFlag, chatID])
+
   return (
-    <div className="flex flex-col justify-between h-full">
+    <div className="flex flex-col justify-between h-full bg-gray-900 text-white">
       <div
         className="flex-1 overflow-y-auto p-4 space-y-4"
         style={{ maxHeight: 'calc(100vh - 160px)' }}
