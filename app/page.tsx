be094de8cb0ca9e4c267e7a62d75edb81c6353cf
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-31 07:24:30 +0500

    added the shhet for responsive

--- app/page.tsx
+++ app/page.tsx
@@ -18,30 +18,32 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 export default function Home() {
   const { chatID } = useUserContext()
   return (
-    <div className=" bg-gray-900 min-h-screen pt-3  ">
-      <Sheet>
-        <SheetTrigger>
-          {' '}
-          <div className=" border-white rounded-lg p-1 w-4">
-            <GiHamburgerMenu className="text-white" />
-          </div>{' '}
-        </SheetTrigger>
-        <SheetContent side={'left'} className=" bg-gray-900">
-          <UserList />
-        </SheetContent>
-      </Sheet>
+    <div className=" bg-gray-900 min-h-screen   ">
+      <div className=" md:hidden p-3">
+        <Sheet>
+          <SheetTrigger>
+            {' '}
+            <div className=" border-white md:hidden  rounded-lg p-1 w-4">
+              <GiHamburgerMenu className="text-white" />
+            </div>{' '}
+          </SheetTrigger>
+          <SheetContent side={'left'} className=" bg-gray-900">
+            <UserList />
+          </SheetContent>
+        </Sheet>
+      </div>
       <div className="flex">
-        {/* <div className=" bg-gray-800 text-white w-4/12 md:w-2/12">
-        <UserList />
-      </div> */}
-        <div className="bg-gray-900 w-full md:w-10/12 p-4 mx-auto  shadow-lg">
+        <div className=" bg-gray-800 text-white hidden md:block min-h-screen md:w-2/12">
+          <UserList />
+        </div>
+        <div className="bg-gray-900 w-full md:w-10/12 p-4 mx-auto min-h-screen  shadow-lg">
           {chatID ? (
             <ChatList />
           ) : (
             <div className="flex flex-col items-center justify-center h-full">
               <FaComments className="text-blue-400 text-4xl mb-4" />
               <p className="text-gray-300 text-lg">No chats available</p>
-              <p className="text-gray-400">
+              <p className="text-gray-400 text-xs">
                 Start a conversation to see your messages here!
               </p>
             </div>
