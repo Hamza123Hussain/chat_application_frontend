427e9b88acc68dc03cfa3e139b470a8571577fd8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-01 14:47:24 +0500

    save these

--- utils/SocketFile.ts
+++
@@ -1,9 +0,0 @@
-// socket.ts
-import { io, Socket } from 'socket.io-client'
-
-const SOCKET_SERVER_URL = 'http://localhost:5000'
-
-// Create a single instance of Socket.IO
-const socket: Socket = io(SOCKET_SERVER_URL)
-
-export default socket
